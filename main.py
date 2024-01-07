76a08ec193713ead6557925ad24a983dc98956ab
qjasn <1766684296@qq.com>
2024-01-07 12:16:48 +0800

    Update

---
+++ main.py
@@ -0,0 +1,14 @@
+import flet as ft
+from lib.control import AppControl
+global LOAD_PAGE
+
+
+def main(page: ft.Page):
+    _AppControl = AppControl(page)
+    _AppControl.init()
+    page.on_route_change = _AppControl.route.change_route
+    page.on_view_pop = _AppControl.route.view_pop
+    page.on_resize = _AppControl.on_resize
+
+
+ft.app(target=main)
---
+++ ui/home.py
@@ -0,0 +1,72 @@
+import io
+
+from flet import *
+from flet.matplotlib_chart import MatplotlibChart
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+matplotlib.use("svg")
+
+
+def home_page(page, navbar):
+    fig, ax = plt.subplots()
+
+    fruits = ["apple", "blueberry", "cherry", "orange"]
+    counts = [40, 100, 30, 55]
+    bar_labels = ["red", "blue", "_red", "orange"]
+    bar_colors = ["tab:red", "tab:blue", "tab:red", "tab:orange"]
+
+    ax.bar(fruits, counts, label=bar_labels, color=bar_colors)
+
+    ax.set_ylabel("fruit supply")
+    ax.set_title("Fruit supply by kind and color")
+    ax.legend(title="Fruit color")
+    s = io.StringIO()
+    fig.savefig(s, format="svg", transparent=True)
+    svg = s.getvalue()
+    matplotlib_chart = Image(src=svg, visible=True)
+
+    def show_chart(e):
+        matplotlib_chart.visible = True
+        page.update()
+
+    content = SafeArea(
+        content=Row(
+            [
+                navbar,
+                Column(
+                    [
+                        Text("Hello world"),
+                        ElevatedButton("Show Chart", on_click=show_chart),
+                    ]),
+                Container(
+                    matplotlib_chart,
+                    bgcolor=colors.YELLOW,
+
+                )
+            ],
+            expand=True
+        )
+    ) if page.width > 550 else SafeArea(
+        content=Column(
+
+                [
+                    Container(
+                        matplotlib_chart,
+                        bgcolor=colors.YELLOW,
+
+                    ),
+                    Row(
+                        [
+                            Text("Hello world"),
+                            ElevatedButton("Show Chart", on_click=show_chart),
+                        ])
+                ],
+        )
+    )
+    view = View(
+        "/home",
+        controls=[content]
+    )
+    return content
