5e5d9bffec2c7ee419ceec35d94e7d901592df07
qjasn <73731689+qjasn@users.noreply.github.com>
2024-01-20 21:41:10 +0800

    更新 control.py 注释

--- basic/control.py
+++ basic/control.py
@@ -3,30 +3,35 @@ import flet as ft
 from basic.app_str import UString
 from basic.navigation import Navigation
 
-
+# 总控Class
 class AppControl:
     lists = []
 
     def __init__(self, page: ft.Page):
         self.page = page
+        # 实例化导航Class
         self.route = Navigation(page)
 
     def init(self):
+        # 初始化整个页面
         _page = self.page
         UString.width = self.page.width
         UString.height = self.page.height
-        _page.title = "graphical calc"
+        _page.title = "graphical calc" # 设置应用标题
+        # 查看是否自定义设置了UI显示类型
         if _page.client_storage.get("fx.darkMode") is None:
             _page.client_storage.set("fx.darkMode", "SYSTEM")
         _page.theme_mode = UString.darkMode[_page.client_storage.get("fx.darkMode")]
+        # 初始化路由
         self.route.init_route()
         _page.update()
 
     def on_resize(self, resize):
-        UString.resize = True
+        # 页面分辨率更新时触发
+        UString.resize = True # 设置 分辨率更新模式 为True
         UString.width = self.page.width
         UString.height = self.page.height
         _page = self.page
-        self.route.update_ui()
+        self.route.update_ui() # 更新页面UI
         UString.resize = False
         _page.update()
