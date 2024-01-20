0c83ced16a2ff9d11f4b2500d79c7a1b86077e2d
qjasn <73731689+qjasn@users.noreply.github.com>
2024-01-20 21:56:21 +0800

    更新 app_str.py

--- basic/app_str.py
+++ basic/app_str.py
@@ -4,8 +4,8 @@ from flet_core import Page, ThemeMode
 class UString:
     lists = [] # 所有的结构化函数，内部item形如：{"name":"f","args":"x",text:"return x"}，其中name是函数名称，args为函数参数，即f（x）中的x，text为函数返回的表达式，即f(x)=x中等号右侧的部分
     t = 0 # 为默认函数下表，即每次新建函数时函数名称的下标，0为空
-    f_n = ["f", "g", "h", "p", "q", "r", "s", "t"] # 默认函数，这些关键字是在新建函数时name字段的默认值
-    a_e = [] # 已经存在的函数名称，防止重复命名函数
+    f_n = ["f", "g", "h", "p", "q", "r", "s", "t"] # 默认函数，这些关键字是在新建函数时name字段的默认值,f_n即function name
+    a_e = [] # 已经存在的函数名称，防止重复命名函数，a_e即already exist
     width = 500 # 页面宽度，默认为500
     height = 600 # 页面高度，默认为600
     step = 25 # 步长，用于给出页面宽度像素与函数图像x轴的值的关系
