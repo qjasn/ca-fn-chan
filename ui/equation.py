7239db8f5b9a40142a33a7c51837d15d260943d4
qjasn <73731689+qjasn@users.noreply.github.com>
2024-01-20 21:49:30 +0800

    更新 equation.py 注释

--- ui/equation.py
+++ ui/equation.py
@@ -5,7 +5,7 @@ from matplot.basic_graphic import MatPlotUi
 from matplot.draw_user_function import DrawUserFunction
 from matplot.latex import Latex
 
-
+# 该类主要负责控制渲染Latex
 class EquationUI:
     delete = []
 
@@ -16,27 +16,30 @@ class EquationUI:
             "name": name,
             "args": args,
             "text": text
-        }
+        } # 设置此类预备转化为latex的对象，其中说明见 basic/app_str.py
         self.page = page
-        self.latex = Latex(name=name, args=args, text=text, page=page)
-        self.latex.init(subscript)
-        self.latex_image = self.latex.output_svg()
+        self.latex = Latex(name=name, args=args, text=text, page=page) # 将参数转化为latex
+        self.latex.init(subscript) # 初始化Latex类
+        self.latex_image = self.latex.output_svg() # 获取渲染后的svg图像
 
     def on_change(self, e):
         print(self)
 
     def on_click(self, _list=None, element=None):
-        UString.lists.remove(_list)
+        # 该函数在点击delete时调用
+        UString.lists.remove(_list) # 从lists删除对应的结构化函数
         print(UString.a_e)
-        UString.a_e.remove(_list["name"])
-        element.content.controls.remove(self.ui)
-        UString.matplot_chart.draw(True)
+        UString.a_e.remove(_list["name"]) # 删除存在的函数名称
+        element.content.controls.remove(self.ui) # 从页面删除此元素
+        UString.matplot_chart.draw(True) # 清除函数图像
+        # 在移除之后重新绘制函数图像
         for content in UString.lists:
             DrawUserFunction(content, self.page).draw(UString.matplot_chart.return_ax())
-        UString.matplot_chart.update_draw()
+        UString.matplot_chart.update_draw() #更新UI
         self.page.update()
 
     def create_ui(self, _list, element):
+        # 构建UI
         self.ui = Row(
             [
                 Stack([
