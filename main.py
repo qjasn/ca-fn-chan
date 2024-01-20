aa6701243f90760bce4ce0c1b92286a029b2c9c0
qjasn <73731689+qjasn@users.noreply.github.com>
2024-01-20 21:19:03 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from basic.control import AppControl
 
 
 def main(page: ft.Page):
+    # 实例化总控类
     _AppControl = AppControl(page)
     _AppControl.init()
     UString(page)
+    # 分别设置flet的响应项目
     page.on_route_change = _AppControl.route.change_route
     page.on_view_pop = _AppControl.route.view_pop
     page.on_resize = lambda e: (
