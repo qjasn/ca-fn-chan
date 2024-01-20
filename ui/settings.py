946fd8af1d0cdec4dd71582765b2b77ddbc3f29c
qjasn <73731689+qjasn@users.noreply.github.com>
2024-01-21 01:40:41 +0800

    更新 settings.py

--- ui/settings.py
+++ ui/settings.py
@@ -5,15 +5,16 @@ from basic.app_str import UString
 
 def settings_page(_page, navbar):
     def dark_mode_change(e):
+        # 单选更新后触发
         _mode = _page.client_storage.get("fx.darkMode")
         _page.client_storage.set("fx.darkMode", e.control.value)
         _page.theme_mode = UString.darkMode[e.control.value]
         _page.update()
 
     dark_mode_ui = RadioGroup(Column([
-        Radio(value="SYSTEM", label="System"),
-        Radio(value="LIGHT", label="Light"),
-        Radio(value="DARK", label="Dark")
+        Radio(value="SYSTEM", label="跟随系统"),
+        Radio(value="LIGHT", label="明亮"),
+        Radio(value="DARK", label="暗黑")
 
     ], alignment=MainAxisAlignment.START),
         value=_page.client_storage.get("fx.darkMode"),
@@ -21,8 +22,8 @@ def settings_page(_page, navbar):
     )
     content = Column(
         [Column([
-            ListTile(title=Text("Settings")),
-            ListTile(leading=Icon(icons.DARK_MODE), title=Text("DarkMode")),
+            ListTile(title=Text("设置")),
+            ListTile(leading=Icon(icons.DARK_MODE), title=Text("暗黑模式")),
             Divider(height=1),
             dark_mode_ui
         ],
