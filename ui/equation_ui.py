b1d1aaed03a8e818a47514896ab18678f721f0f3
qjasn <1766684296@qq.com>
2024-01-24 00:12:33 +0800

    Update

--- ui/equation_ui.py
+++ ui/equation_ui.py
@@ -1,4 +1,22 @@
+from sympy import *
+
 
 class EquationUI:
-    def __init__(self):
-        print(self)
+    def __init__(self, equ: str, args="x"):
+        _solve = ""
+        self.equation = equ
+        self.map = {}
+        args = args.replace(" ", "")
+        for i in args.split(","):
+            exec("result = symbols('{}')".format(i))
+            self.map[i] = locals()["result"]
+        for i in args.split(","):
+            _equ = equ.replace(i, 'self.map["{}"]'.format(i))
+        self.pares = parse_expr(equ,transformations="all")
+        self.latex = latex(self.pares)
+        print(str(self.latex))
+        for i in args.split(","):
+            _solve = _solve + "self.map['{}'],".format(i)
+        exec("result = solve(self.pares,({}),dict=True)".format(_solve))
+        self.solve = locals()["result"]
+        print(self.solve)
