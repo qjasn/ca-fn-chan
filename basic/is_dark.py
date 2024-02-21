26c5bfef41d645bc86b7eaf1350d29235a4c2f74
qjasn <1766684296@qq.com>
2024-02-22 02:54:56 +0800

    python repl

--- basic/is_dark.py
+++ basic/is_dark.py
@@ -12,3 +12,15 @@ def is_dark(page: ft.Page):
         return True
     else:
         return False
+
+
+def is_closed(text: str) -> bool:
+    stack = []
+    brackets = {')': '(', ']': '[', '}': '{'}
+    for char in text:
+        if char in brackets.values():
+            stack.append(char)
+        elif char in brackets.keys():
+            if brackets[char] != stack.pop():
+                return False
+    return True
--- ui/python_page.py
+++ ui/python_page.py
@@ -1,11 +1,14 @@
 import asyncio
 import io
 import sys
+import time
+from os import truncate
 from threading import Thread
 
 from flet import *
 
 from basic.app_str import UString
+from basic.is_dark import is_closed
 
 
 class PythonRepl(UserControl):
@@ -20,46 +23,82 @@ class PythonRepl(UserControl):
         self.ioString_err = io.StringIO()
         self.ioString_in = io.StringIO()
         self.temp_io = [self.ioString_out.getvalue(), self.ioString_err.getvalue(), self.ioString_err.getvalue()]
-        self.display = Column(scroll=ScrollMode.ALWAYS)
+        self.display = Column([
+            Text("Python " + sys.version + " REPl")
+        ],scroll=ScrollMode.ALWAYS)
         self.running = False
+        self.input_e = TextField(
+            border=InputBorder.NONE,
+            on_submit=self.on_submit
+        )
+        self.input_t = Text("> ")
+        self.input = Row([self.input_t, self.input_e])
+        self.code = ""
+
+    async def on_submit(self, e):
+        value = str(e.control.value)
+        self.input_e.value = ""
+        self.display.controls.remove(self.input)
+        self.display.controls.append(Text(self.input_t.value + value + "\n"))
+        await self.update_async()
+        if value == "":
+            await self.run()
+            self.code = ""
+            self.input_t.value = "> "
+            self.display.controls.append(self.input)
+        elif value.endswith(":"):
+            self.code += "\n{}".format(value)
+            self.input_t.value = "... "
+            self.display.controls.append(self.input)
+        elif not is_closed(self.code + "\n" + value):
+            self.code += "\n{}".format(value)
+            self.input_t.value = "... "
+            self.display.controls.append(self.input)
+        else:
+            self.code = value
+            await self.run()
+            self.code = ""
+            self.input_t.value = "> "
+            self.display.controls.append(self.input)
+        await self.update_async()
+        await self.input_e.focus_async()
 
     async def did_mount_async(self):
         self.running = True
-        for i in self.ioString_out.getvalue().split("\n"):
-            self.display.controls.append(Text(i))
         self.temp_io = [self.ioString_out.getvalue(), self.ioString_err.getvalue(), self.ioString_err.getvalue()]
+        self.display.controls.append(self.input)
         await self.update_async()
         # noinspection PyAsyncCall
         asyncio.create_task(self.on_write())
 
     async def on_write(self):
-        print("on write")
         while self.running:
-            if self.ioString_out.getvalue() != self.temp_io[0]:
-                self.temp_io[0] = self.ioString_out.getvalue()
-                self.display.controls.append(Text(self.temp_io[0].split("\n")[-2]))
+            await asyncio.sleep(0.01)
+            if self.ioString_out.getvalue() != "":
+                temp = self.ioString_out.getvalue()
+                self.ioString_out.close()
+                self.display.controls.append(Text(temp))
                 await self.update_async()
-            if self.ioString_err.getvalue() != self.temp_io[1]:
-                self.temp_io[1] = self.ioString_err.getvalue()
-                self.display.controls.append(Text(self.temp_io[1].split("\n")[-2], color=colors.RED))
+                self.ioString_out = io.StringIO()
+            if self.ioString_err.getvalue() != "":
+                temp = self.ioString_err.getvalue()
+                self.ioString_err.close()
+                self.display.controls.append(Text(temp,color=colors.RED))
+                await self.update_async()
+                self.ioString_err = io.StringIO()
                 await self.update_async()
 
     async def will_unmount_async(self):
         self.running = False
-        self.display.controls = []
-        await self.update_async()
 
-    def run(self, code):
+    async def run(self):
         old_stdout, old_stderr = sys.stdout, sys.stderr
         sys.stdout = self.ioString_out
         sys.stderr = self.ioString_err
-        self.code = code
-        thread = Thread(target=self.exec)
-        thread.start()
-        thread.join()
+        await self.exec()
         sys.stdout, sys.stderr = old_stdout, old_stderr
 
-    def exec(self):
+    async def exec(self):
         exec(self.code, PythonRepl._locals, PythonRepl._globals)
 
     def build(self):
@@ -69,18 +108,10 @@ class PythonRepl(UserControl):
 def python_page(_page, navbar):
     if UString.python_repl is None:
         UString.python_repl = PythonRepl()
-        UString.python_repl.ioString_out.write("Python " + sys.version + " REPl")
     repl = UString.python_repl
 
-    input_e = TextField(
-        # icon=icons.KEYBOARD_ARROW_RIGHT,
-        multiline=True,
-        max_lines=5,
-        border=InputBorder.NONE
-    )
-
     content = Column([
-        repl
+        repl,
     ])
 
     return [Row([
