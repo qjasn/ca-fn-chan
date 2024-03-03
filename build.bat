9526840fb1024e3cdebdf8f7304c8a97e7695c80
qjasn <1766684296@qq.com>
2024-03-03 20:41:08 +0800

    重新上传构建文件

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-flet pack --product-name "算方阐" --product-version "beta-0.1.0" --copyright "CaFnChan Copyright (C) 2024 asahi_qin 秦奥楠" --icon .\assets\icon.jpg
+flet pack main.py --icon .\assets\icon.jpg
--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ flet pack ./main.py \
   --product-version "beta-0.1.0" \
   --copyright "CaFnChan Copyright (C) 2024 asahi_qin 秦奥楠" \
   --icon ./assets/icon.jpg
+mv ./dist/main ./dist/ca-fn-chan
+mv ./dist/main.app ./dist/ca-fn-chan.app
+bash ./clean_build.sh
